84c7b79589affa380a6d46041d659c5874e4ba27
TuanPham303 <phamcongtuan303@gmail.com>
2018-06-26 22:27:10 -0700

    first commit

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class HomePage extends React.Component{
+  render () {
+    return (
+      <div className="jumbotron">
+        <h1>Udemy Admin</h1>
+        <p>React, Redux and React router in ES6</p>
+        <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
+      </div>
+    );
+  }
+}
